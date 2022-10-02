554e3529517279e608322d0ae4ec2583a65f7998
unknown <Niromedium@gmail.com>
2022-10-02 17:24:15 +0500

    add Programm Code

---
+++ Code/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] Array = new string[8] {"control", "work", "world", "08", ":)", "lesson", "up", "down"};
+int count = 0;
+for (int i = 0; i < Array.Length; i ++)
+{
+    Console.Write($"{Array[i]},");
+    if (Array[i].Length < 4)
+    {
+        count ++;
+    }
+}
+Console.WriteLine();
+
+string[] Array2 = new string[count];
+int j = 0;
+for (int i = 0; i < Array.Length; i ++)
+{
+    if (Array[i].Length < 4)
+    {
+        Array2[j] = Array[i];
+        Console.Write($"{Array2[j]},");
+        j ++;
+    }
+}
